424498e37101dc47d87b0662d8590fdc7d82090a
cr.zoidberg <crypto.zoidberg@gmail.com>
2024-06-18 23:08:07 +0200

    removed lfs files

--- apple/Package.swift
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:919d61ec5b58301e848bc89bc7c6d81dbeb279944e101ce6a060ae1fed89cd77
-size 2835
--- apple/Tests/ZanoWalletTests/ZanoWalletTests.swift
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:853717bc52ff91d62f55fbde2a998e27be0f21ead96b5c5f11a61e89ddd151f7
-size 3163
